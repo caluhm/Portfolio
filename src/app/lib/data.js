1c6a4d8a69d183d4c7d67a4a45e4ed247ed27722
caluhm <calum.jarvo@gmail.com>
2023-07-31 21:29:41 +0100

    JPEG instead of jpeg... will this work

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -1,11 +1,11 @@
-import Floating1 from "../../../public/images/floating_1.jpeg"
-import Floating2 from "../../../public/images/floating_2.jpeg";
-import Floating3 from "../../../public/images/floating_3.jpeg";
-import Floating4 from "../../../public/images/floating_4.jpeg";
-import Floating5 from "../../../public/images/floating_5.jpeg";
-import Floating6 from "../../../public/images/floating_6.jpeg";
-import Floating7 from "../../../public/images/floating_7.jpeg";
-import Floating8 from "../../../public/images/floating_8.jpeg";
+import Floating1 from "../../../public/images/floating_1.JPEG";
+import Floating2 from "../../../public/images/floating_2.JPEG";
+import Floating3 from "../../../public/images/floating_3.JPEG";
+import Floating4 from "../../../public/images/floating_4.JPEG";
+import Floating5 from "../../../public/images/floating_5.JPEG";
+import Floating6 from "../../../public/images/floating_6.JPEG";
+import Floating7 from "../../../public/images/floating_7.JPEG";
+import Floating8 from "../../../public/images/floating_8.JPEG";
 
 export {
   Floating1,
