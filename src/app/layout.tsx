154d37f5fdd840f8cf470573d98ac271f7bfed3e
caluhm <calum.jarvo@gmail.com>
2023-08-02 00:28:07 +0100

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'calumjarvis.dev',
-  description: 'Hi, I’m Calum, a web developer based in Manchester. Welcome to my personal website.',
-}
+  title: "calumjarvis.dev",
+  description:
+    "Hi, I’m Calum, a web developer based in Manchester. Welcome to my personal website.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
